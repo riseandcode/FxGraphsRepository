711ff7cc777d9877b40e8d27291b20e889a198d5
riseandcode <riseandcode@gmail.com>
2014-03-08 14:33:32 +0200

    Layout fixed. Stats test data for graph added.

--- FxGraphs/Controllers/StatsController.cs
+++ FxGraphs/Controllers/StatsController.cs
@@ -6,15 +6,26 @@ using System.Web.Mvc;
 
 namespace ForexBox.Controllers
 {
-    public class StatsController : ForexBoxController
-    {
-        //
-        // GET: /Stats/
+   public class StatsController : ForexBoxController
+   {
+      //
+      // GET: /Stats/
 
-        public ActionResult Index()
-        {
-            return View();
-        }
+      public ActionResult Index()
+      {
+         return View();
+      }
 
-    }
+      [HttpGet]
+      public ContentResult GetGraphData(string graphType)
+      {
+         string json = "[{ \"year\": \"2003\", \"win\": 13,\"extremum\": \"MIN: 13\",\"loss\": 3},{\"year\": \"2004\",\"win\": 22,\"loss\": 1}]";
+
+         return Content(json, "application/json");
+
+
+
+      }
+   }
+
 }
