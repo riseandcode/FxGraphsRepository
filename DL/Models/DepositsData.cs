5715d4f4d17fb834a74199cf45a10fbd40a5b580
riseandcode <riseandcode@gmail.com>
2014-03-11 17:29:26 +0200

    Added Type to depositsdata table.

---
+++ DL/Models/DepositsData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DL
+{
+    public partial class DepositsData
+    {
+        public DataType IncomeType
+        {
+            get { return ParseDataType(Type); }
+        }
+
+        private DataType ParseDataType(int? type)
+        {
+            switch (type)
+            {
+                case 0:
+                    return DataType.Deposit;
+                case 1:
+                    return DataType.FloatingProfit;
+
+                default:
+                    return DataType.Deposit;
+            }
+        }
+    }
+
+    public enum DataType
+    {
+        Deposit = 0, FloatingProfit
+    }
+}
